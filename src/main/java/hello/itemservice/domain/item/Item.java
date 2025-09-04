3e80aa46c7e134055b48762245fa92fa1f18110c
OllyDI <choiseungho11@hanmail.net>
2025-09-04 21:50:11 +0900

    25.09.04

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,30 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10,000원 넘게 입력해주세요.")
 public class Item {
-
+    
+    // 실무에서는 groups를 잘 사용하지 않음 -> 복잡도가 너무 증가됨
+    @NotNull(groups = UpdateCheck.class)
     private Long id;
+
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
+
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
+
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
